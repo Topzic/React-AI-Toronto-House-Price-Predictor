8761f93577d2665cd16aebe8467f894cfab3e05b
Topzic <ian_cunningham@live.com>
2024-04-20 13:07:42 -0400

    Added Data Training Region on Home

--- react-ui/src/components/Home.jsx
+++ react-ui/src/components/Home.jsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; // Import Link for navigation
 import { Container, Row, Col, Button } from 'react-bootstrap'; // Import components from react-bootstrap
+import region from '../../public/region.png'
 
 function Home() {
 
@@ -15,22 +16,7 @@ function Home() {
             <p className="lead">
               We help you predict house prices based on various factors such as bedrooms, bathrooms, square footage, parking spots, longitude, latitude, and house type.
             </p>
-            <hr className="my-4" />
-            <p>
-              Explore our features:
-            </p>
-            <p className="lead">
-              <Link to="/list">
-                <Button variant="primary" className="m-2">View Predictions</Button>
-              </Link>
-              <Link to="/prediction">
-                <Button variant="info">Make a Prediction</Button>
-              </Link>
-            </p>
-          </div>
-        </Col>
-      </Row>
-      <Row className="mt-5">
+            <Row className="mt-4">
         <Col>
           <h2>Why Choose Us?</h2>
           <p>
@@ -39,6 +25,17 @@ function Home() {
           <p>
             Our user-friendly interface makes it easy for you to explore predictions and make new predictions.
           </p>
+        </Col>
+      </Row>
+        <Link to="/list">
+          <Button variant="primary" className="m-2">View Predictions</Button>
+        </Link>
+        <Link to="/prediction">
+          <Button variant="info">Make a Prediction</Button>
+        </Link>
+            <hr className="my-4" />
+            <img src={region} style={{ width: "75%", marginBottom: 50}}></img>
+          </div>
         </Col>
       </Row>
     </Container>
